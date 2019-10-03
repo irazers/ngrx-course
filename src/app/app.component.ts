c3337d8a4b7d0ce0d3f3f6801f5b2097418d96ae
Cedric Peruzzi <cedric.peruzzi@outlook.com>
2019-10-03 17:02:15 +0200

    section 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {select, Store} from '@ngrx/store';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+
+import { select, Store } from '@ngrx/store';
+
+import { Observable } from 'rxjs';
+
+import { isLoggedIn, isLoggedOut } from './auth.selectors';
+import { AppState } from './reducers';
 
 @Component({
   selector: 'app-root',
@@ -12,20 +16,20 @@ import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Route
 export class AppComponent implements OnInit {
 
     loading = true;
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private store: Store<AppState>) {
 
     }
 
     ngOnInit() {
-
       this.router.events.subscribe(event  => {
         switch (true) {
           case event instanceof NavigationStart: {
             this.loading = true;
             break;
           }
-
           case event instanceof NavigationEnd:
           case event instanceof NavigationCancel:
           case event instanceof NavigationError: {
@@ -38,6 +42,14 @@ export class AppComponent implements OnInit {
         }
       });
 
+      this.isLoggedIn$ = this.store
+        .pipe(
+          select(isLoggedIn)
+        );
+      this.isLoggedOut$ = this.store
+        .pipe(
+          select(isLoggedOut)
+        );
     }
 
     logout() {
