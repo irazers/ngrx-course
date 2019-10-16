e3c52a957e9e4baf779dfe26a0a66f55e0fb3b6c
Cedric Peruzzi <cedric.peruzzi@outlook.com>
2019-10-16 09:41:29 +0200

    continue

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -2,19 +2,27 @@ import { createEntityAdapter, EntityState } from '@ngrx/entity';
 import { createReducer, on } from '@ngrx/store';
 
 import { CourseActions } from '../action-types';
-import { Course } from '../model/course';
+import { compareCourses, Course } from '../model/course';
 
 // tslint:disable-next-line:no-empty-interface
 export interface CoursesState extends EntityState<Course> {
+  allCoursesLoaded: boolean;
 }
 
-export const adapter = createEntityAdapter<Course>();
+export const adapter = createEntityAdapter<Course>({
+  sortComparer: compareCourses
+});
 
-export const initialCourseState = adapter.getInitialState();
+export const initialCourseState = adapter.getInitialState({
+  allCoursesLoaded: false
+});
 
 export const coursesReducer = createReducer(
   initialCourseState,
-  on(CourseActions.allCoursesLoaded, (state, action) => adapter.addAll(action.courses, state))
+  on(CourseActions.allCoursesLoaded, (state, action) => adapter.addAll(
+    action.courses,
+    {...state, allCoursesLoaded: true}
+  ))
 );
 
 export const {selectAll} = adapter.getSelectors();
